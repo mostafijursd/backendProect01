e5842c293bccf330ac01a0dafa1c7082db36b4ab
Mostafijur Rahaman <148426873+mostafijursd@users.noreply.github.com>
2024-05-13 22:11:03 +0530

    some  commit

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+ require('dotenv').config()
+ const express = require('express')
+ const app = express()
+ const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+ app.get('/', (req, res) => {
+     res.send('Hello World!')
+ })
 
-app.get('/login', (req, res) => {
-    res.send('Login page ')
-})
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+ app.get('/login', (req, res) => {
+     res.send('Login page ')
+ })
+ app.listen(process.env.PORT, () => {
+     console.log(`Example app listening on port ${port}`)
+ })
